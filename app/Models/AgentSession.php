5ddac5f85e169822dd857b22587d636adb50d495
laupwing <laupwing@gmail.com>
2025-09-25 16:00:21 +0200

    Add messages relationship to AgentSession model

--- app/Models/AgentSession.php
+++ app/Models/AgentSession.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AgentSession extends Model
 {
@@ -28,5 +29,10 @@ class AgentSession extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
     }
 }
