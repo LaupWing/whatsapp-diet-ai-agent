a1cd85c409e832ed86ef702f6ee3336491292ced
laupwing <laupwing@gmail.com>
2025-09-25 16:05:06 +0200

    Add WeighIn model with fillable attributes and user relationship

--- app/Models/WeighIn.php
+++ app/Models/WeighIn.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WeighIn extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'date',
+        'weight_kilogram',
+        'body_fat_percentage'
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'weight_kg' => 'float',
+        'body_fat_percentage' => 'float',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
