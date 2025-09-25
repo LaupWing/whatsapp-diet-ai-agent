221d8d1cc84481bd4dc216ff044230077ed62275
laupwing <laupwing@gmail.com>
2025-09-25 16:07:07 +0200

    Refactor ResolveUserByPublicId middleware to handle user resolution by public ID

--- app/Http/Middleware/ResolveUserByPublicId.php
+++ app/Http/Middleware/ResolveUserByPublicId.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +16,16 @@ class ResolveUserByPublicId
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $publicId = $request->query('public_id', $request->input('public_id'));
+
+        abort_unless($publicId, 422, 'public_id is required');
+
+        $user = User::where('public_id', $publicId)->first();
+        abort_unless($user, 404, 'User not found');
+
+        // attach once; controllers can grab it
+        $request->attributes->set('user', $user);
+
         return $next($request);
     }
 }
