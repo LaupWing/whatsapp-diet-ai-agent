c3f971dc40d38aa222f52232a8929cf69a42c08f
laupwing <laupwing@gmail.com>
2025-09-26 09:35:44 +0200

    Add user.public middleware to diet routes for access control

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::middleware('user.public')->group(function () {
+    Route::prefix('diet')->group(function () {
+        // Route::get('/macros/today', [MealsController::class, 'macrosToday']);
+        // // POST /diet/food_entries
+        // Route::post('/food_entries', [MealsController::class, 'store']);
+    });
+});
