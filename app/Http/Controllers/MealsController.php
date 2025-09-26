d69f59d0b20946ab5335bf362600d240f7115162
laupwing <laupwing@gmail.com>
2025-09-26 09:37:43 +0200

    Implement meal retrieval methods in MealsController and update API routes

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MealResource;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MealsController extends Controller
 {
-    //
+    /**
+     * GET /diet/meals
+     */
+    public function index(Request $request): JsonResponse
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->attributes->get('user');
+
+        $meals = $user->meals()
+            ->with('items')
+            ->forDate($request->date)
+            ->latest('date')
+            ->latest('id')
+            ->when($request->filled('limit'), fn($q) => $q->limit($request->limit))
+            ->get();
+
+        return response()->json([
+            'meals' => MealResource::collection($meals)
+        ]);
+    }
+
+    /**
+     * GET /diet/meals/today
+     */
+    public function today(Request $request): JsonResponse
+    {
+        /** @var \App\Models\User $user */
+        $user = $request->attributes->get('user');
+
+        $meals = $user->meals()
+            ->with('items')
+            ->today()
+            ->oldest('id')
+            ->get();
+
+        return response()->json([
+            'date' => today()->toDateString(),
+            'meals' => MealResource::collection($meals)
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MealsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +11,8 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('user.public')->group(function () {
     Route::prefix('diet')->group(function () {
-        // Route::get('/macros/today', [MealsController::class, 'macrosToday']);
-        // // POST /diet/food_entries
-        // Route::post('/food_entries', [MealsController::class, 'store']);
+        Route::get('/macros/today', [MealsController::class, 'macrosToday']);
+        // POST /diet/food_entries
+        Route::post('/food_entries', [MealsController::class, 'store']);
     });
 });
