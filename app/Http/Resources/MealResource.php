fa9c0e5efaeab1b1cef6ccb4fd6e8a16d5c36216
laupwing <laupwing@gmail.com>
2025-09-30 10:48:50 +0200

    Add totals calculation for calories and macronutrients in MealResource

--- app/Http/Resources/MealResource.php
+++ app/Http/Resources/MealResource.php
@@ -20,6 +20,12 @@ class MealResource extends JsonResource
             'source' => $this->source,
             'notes' => $this->notes,
             'items' => MealItemResource::collection($this->whenLoaded('items')),
+            'totals' => [
+                'calories' => $this->items->sum('total_calories'),
+                'protein_grams' => $this->items->sum('total_protein_grams'),
+                'carbs_grams' => $this->items->sum('total_carbs_grams'),
+                'fat_grams' => $this->items->sum('total_fat_grams'),
+            ],
         ];
     }
 }
